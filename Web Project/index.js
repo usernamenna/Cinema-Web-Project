f35187bf81b2738e501ed68c7b13c8694c806ee2
MahmoudIbrahim1012 <58787180+MahmoudIbrahim1012@users.noreply.github.com>
2020-09-20 20:40:53 +0200

    Add files via upload

--- Web Project/index.js
+++ Web Project/index.js
@@ -16,45 +16,43 @@ function readMore() {
     }
 }
 let sliderImages = document.querySelectorAll('.slide'),
-        arrowRight = document.querySelector('#arrow-right'),
-        arrowLeft = document.querySelector('#arrow-left'),
-        current = 0;
+    arrowRight = document.querySelector('#arrow-right'),
+    arrowLeft = document.querySelector('#arrow-left'),
+    current = 0;
 // clear all images
-        function reset(){
-            for(let i=0;i< sliderImages.length;i++){
-                sliderImages[i].style.display = 'none';
-            }
-        }
-        // initialize slider
-       function startSlide() {
-           reset();
-           sliderImages[0].style.display = 'block';
-       }
-       // show previous
-       function slideLeft()
-       {
-           reset();
-           sliderImages[current - 1].style.display = 'block';
-           current--;
-       }
-       // show next
-       function slideRight()
-       {
-           reset();
-           sliderImages[current + 1].style.display = 'block';
-           current++;
-       }
-       // left arrow click
-       arrowLeft.addEventListener('click', function(){
-           if(current == 0){
-               current = sliderImages.length;
-           }
-           slideLeft();
-       })
-       arrowRight.addEventListener('click', function(){
-           if(current == sliderImages.length - 1){
-               current = -1
-           }
-           slideRight();
-       })
-       startSlide();
+function reset() {
+    for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = 'none';
+    }
+}
+// initialize slider
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = 'block';
+}
+// show previous
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = 'block';
+    current--;
+}
+// show next
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = 'block';
+    current++;
+}
+// left arrow click
+arrowLeft.addEventListener('click', function () {
+    if (current == 0) {
+        current = sliderImages.length;
+    }
+    slideLeft();
+})
+arrowRight.addEventListener('click', function () {
+    if (current == sliderImages.length - 1) {
+        current = -1
+    }
+    slideRight();
+})
+startSlide();
