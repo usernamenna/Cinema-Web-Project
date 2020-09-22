c60e9d85fe1060bfc2d8344a6270d3bf8b34e733
mariammahmoud18 <mariammahmoud18@users.noreply.github.com>
2020-09-22 15:12:25 +0200

    Update sign.js

--- Web Project/sign.js
+++ Web Project/sign.js
@@ -13,12 +13,23 @@ function validatePassword() {
 password.onchange = validatePassword;
 confirmPassword.onkeyup = validatePassword;
 
+
+function welcomeUser() {
+    var username = document.getElementById("username").value;
+    if (username == "") {
+        alert("Username must not be left empty");
+        return false;
+    }
+    alert("Welcome back, " + username + "!");
+}
+
 function checkAdmin() {
     var adminUsername = document.getElementById("username");
     var adminPassword = document.getElementById("password");
     if (adminUsername.value == "admin" && adminPassword.value == "Admin123") {
         setTimeout(function() {window.location.href="admin.html"},0);
     } 
+    welcomeUser();
     }
 
 
@@ -30,12 +41,4 @@ function checkAdmin() {
 }*/
 
 
-function welcomeUser() {
-    var username = document.getElementById("username").value;
-    if (username == "") {
-        alert("Username must not be left empty");
-        return false;
-    }
-    alert("Welcome back, " + username + "!");
-}
 
