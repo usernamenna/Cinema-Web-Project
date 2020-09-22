33da40de9c2ca8fc1802a9ee2cbb2b0b9989afa1
MahmoudIbrahim1012 <58787180+MahmoudIbrahim1012@users.noreply.github.com>
2020-09-22 13:06:05 +0200

    Fixed a small bug
    User would get a welcome back message even if no username was entered

--- Web Project/sign.js
+++ Web Project/sign.js
@@ -1,45 +1,42 @@
 //Sign up
-var password = document.getElementById("password2")
-, confirm_password = document.getElementById("password3");
+var password = document.getElementById("password2"),
+    confirm_password = document.getElementById("password3");
 
-function validatePassword(){
-  if(password.value != confirm_password.value) {
-    confirm_password.setCustomValidity("Passwords Don't Match");
-  } else {
-    confirm_password.setCustomValidity('');
-  }
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+        confirm_password.setCustomValidity('');
+    }
 }
 
 //Admin page
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
 
-function checkAdmin()
-{
-  var adminUsername= document.getElementById("username");
-  var adminPassword= document.getElementById("password");
-  var admin=false;
-  if(adminUsername.value=="admin" && adminPassword.value=="Admin123")
-  {
-    admin=true;
-    window.location.href="admin.html";
-  }
-  else {
-    admin=false;
-  }
+function checkAdmin() {
+    var adminUsername = document.getElementById("username");
+    var adminPassword = document.getElementById("password");
+    var admin = false;
+    if (adminUsername.value == "admin" && adminPassword.value == "Admin123") {
+        admin = true;
+        window.location.href = "admin.html";
+    } else {
+        admin = false;
+    }
 }
 
 
-function changeMode() 
-{
+function changeMode() {
 
-  let element = document.body;
-  element.classList.toggle("dark-mode");
+    let element = document.body;
+    element.classList.toggle("dark-mode");
 }
 
-function welcomeUser()
-{
-  var username=document.getElementById("username").value;
-  if(username!=null){
-  alert("Welcome back, "+username + "!");}
+function welcomeUser() {
+    var username = document.getElementById("username").value;
+    if (username == "") {
+        alert("Username must not be left empty");
+    }
+    alert("Welcome back, " + username + "!");
 }
